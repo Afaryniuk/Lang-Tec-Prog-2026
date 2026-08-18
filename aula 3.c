18413bbe303e251a7afffb14012a8f7a7f870e9e
Afaryniuk <penelopeafaryniuk@gmail.com>
2026-08-17 21:52:38 -0300

    Update aula 3.c

--- aula 3.c
+++ aula 3.c
@@ -26,6 +26,32 @@ int main(int argc, char *argv[]) {
     scanf("%lf", &valor);
     
     printf("Notacao cientifica: %e \n", valor);
+
+	// exercicio 3
+    int n, res;
+    int bit_64, bit_32, bit_16, bit_8, bit_4, bit_2, bit_1;
+	printf("insira o valor a ser convertido: ");
+    scanf("%d", &n);// -> 41
+    	
+    bit_64 = n%2;//-> 1
+    res = n/2;//-> 20
     
-	return 0;
-}
+    bit_32 = res%2;//-> 0
+    res = res/2;//-> 20
+    	
+    bit_16 = res%2;//->0
+    res = res/2;
+   		
+    bit_8 = res%2;//->1
+    res = res/2;
+    
+    bit_4 = res%2;
+    res = res/2;
+    	
+	bit_2 = res%2;
+    res = res/2;
+    		
+   printf("O numero %d em bin = %d%d%d%d%d%d%d", n, res%2, bit_2, bit_4, bit_8, bit_16, bit_32, bit_64 );
+    
+    return 0
+		}
